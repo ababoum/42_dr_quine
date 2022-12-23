3b2aa3f0b0f19ee4508010e570ed8ded7dd59d65
ababoum <ababou237@hotmail.com>
2022-12-23 14:59:22 +0100

    part2 C

--- part2/C/Grace_kid.c
+++ part2/C/Grace_kid.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 // Hello World
-#define CODE "#include <stdio.h>%c// Hello World%c#define CODE %c%s%c%c#define FT()int main(void){ FILE *f = fopen(%cGrace_kid.c%c, %cw%c); if (!f) return (1); fprintf(f, CODE, 10, 10, 34, CODE, 34, 10, 34, 34, 34, 34, 10); fclose(f);}%cFT()"
-#define FT()int main(void){ FILE *f = fopen("Grace_kid.c", "w"); if (!f) return (1); fprintf(f, CODE, 10, 10, 34, CODE, 34, 10, 34, 34, 34, 34, 10); fclose(f);}
+#define INCLUDE "#include <stdio.h>"
+#define CODE "%s%c// Hello World%c#define INCLUDE %c#include <stdio.h>%c%c#define CODE %c%s%c%c#define FT() int main(void) { FILE *f = fopen(%cGrace_kid.c%c, %cw%c); if (!f) return 1; fprintf(f, CODE, INCLUDE, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 34, 34, 34, 34, 10); fclose(f);}%cFT()"
+#define FT() int main(void) { FILE *f = fopen("Grace_kid.c", "w"); if (!f) return 1; fprintf(f, CODE, INCLUDE, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 34, 34, 34, 34, 10); fclose(f);}
 FT()
