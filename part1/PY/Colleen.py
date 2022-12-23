4c46ac9b508fb0f73d1c2b87a7c1039bcc26c3f7
M'hamed Noé Ababou <71515030+ababoum@users.noreply.github.com>
2022-12-24 00:38:13 +0100

    Update Colleen.py

--- part1/PY/Colleen.py
+++ part1/PY/Colleen.py
@@ -1,7 +1,8 @@
 def ft():
     return 42
-
+# This program will print its own source when run.
 if __name__ == "__main__":
-    s = 'def ft():{}    return 42{}{}if __name__ == "__main__":{}    s = {}{}    ft(){}    print(s.format(chr(10), chr(10), chr(10), chr(10), repr(s), chr(10),  chr(10)))'
+    s = 'def ft():{}    return 42{}# This program will print its own source when run.{}{}if __name__ == "__main__":{}    s = {}{}    ft(){}    # Comment bis{}    print(s.format(chr(10), chr(10), chr(10), chr(10), chr(10), repr(s), chr(10), chr(10), chr(10)))'
     ft()
-    print(s.format(chr(10), chr(10), chr(10), chr(10), repr(s), chr(10),  chr(10)))
+    # Comment bis
+    print(s.format(chr(10), chr(10), chr(10), chr(10), chr(10), repr(s), chr(10), chr(10), chr(10)))
